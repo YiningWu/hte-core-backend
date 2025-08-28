808292f5e7cba07ad76cda2528ee3723f5c9a4ff
TeejayAsihan <torres33@live.cn>
2025-08-28 20:40:03 +0800

    更新

--- libs/shared/src/filters/global-exception.filter.ts
+++ libs/shared/src/filters/global-exception.filter.ts
@@ -5,14 +5,13 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { Response } from 'express';
 import { ApiResponse } from '../types/common.types';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
+    const response = ctx.getResponse();
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
     let message = '服务器内部错误';
@@ -79,6 +78,6 @@ export class GlobalExceptionFilter implements ExceptionFilter {
       data: null,
     };
 
-    response.status(status).json(errorResponse);
+    response.status(status).send(errorResponse);
   }
 }
